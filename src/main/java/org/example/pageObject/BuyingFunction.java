9795d6b3a26e6b22b7bfaa3ace05f1324845780e
AndersenASG <andersenasg88@gmail.com>
2023-08-12 04:53:50 +0700

    finish buying feature

--- src/main/java/org/example/pageObject/BuyingFunction.java
+++ src/main/java/org/example/pageObject/BuyingFunction.java
@@ -18,6 +18,52 @@ public class BuyingFunction {
 //    Locator
     @FindBy(xpath = "//a[@id='nava']")
     private WebElement DBLogo;
+    @FindBy(xpath = "//h5[@id='signInModalLabel']")
+    private WebElement signupTitle;
+    @FindBy(xpath = "//input[@id='sign-username']")
+    private WebElement signupUser;
+    @FindBy(xpath = "//input[@id='sign-password']")
+    private WebElement signupPass;
+    @FindBy(xpath = "//button[.='Sign up']")
+    private WebElement signupBtn;
+    @FindBy(xpath = "//h5[@id='logInModalLabel']")
+    private WebElement loginTitle;
+    @FindBy(xpath = "//input[@id='loginusername']")
+    private WebElement loginUser;
+    @FindBy(xpath = "//input[@id='loginpassword']")
+    private WebElement loginPass;
+    @FindBy(xpath = "//button[.='Log in']")
+    private WebElement loginBtn;
+    @FindBy(xpath = "//h2[@class='name']")
+    private WebElement productTitle;
+    @FindBy(xpath = "//a[.='Add to cart']")
+    private WebElement cartBtn;
+    @FindBy(xpath = "//h2[.='Products']")
+    private WebElement cartTitle;
+    @FindBy(xpath = "//button[@class='btn btn-success']")
+    private WebElement orderBtn;
+    @FindBy(xpath = "//h5[@id='orderModalLabel']")
+    private WebElement orderTitle;
+    @FindBy(xpath = "//input[@id='name']")
+    private WebElement orderName;
+    @FindBy(xpath = "//input[@id='country']")
+    private WebElement orderCountry;
+    @FindBy(xpath = "//input[@id='city']")
+    private WebElement orderCity;
+    @FindBy(xpath = "//input[@id='card']")
+    private WebElement orderCard;
+    @FindBy(xpath = "//input[@id='month']")
+    private WebElement orderMonth;
+    @FindBy(xpath = "//input[@id='year']")
+    private WebElement orderYear;
+    @FindBy(xpath = "//button[.='Purchase']")
+    private WebElement purchaseBtn;
+    @FindBy(xpath = "//p[@class='lead text-muted ']")
+    private WebElement purchaseConfirm;
+    @FindBy(xpath = "//button[@class='confirm btn btn-lg btn-primary']")
+    private WebElement OKBtn;
+    @FindBy(xpath = "//tbody[@id='tbodyid']")
+    private WebElement itemTable;
 
 
 //    Custom Locator
@@ -25,8 +71,16 @@ public class BuyingFunction {
         if (navbarBtn.equals("Home")) {
             return By.xpath("//a[.='" +navbarBtn+ " (current)']");
         } else {
-            return By.xpath("//a[.='" + navbarBtn + "']");
+            return By.xpath("//a[.='" +navbarBtn+ "']");
         }
+    }
+
+    public By catFilter (String catBtn) {
+        return By.xpath("//a[.='" +catBtn+ "']");
+    }
+
+    public By productLoc (String product) {
+        return By.xpath("//a[.='" +product+ "']");
     }
 
 
@@ -38,5 +92,111 @@ public class BuyingFunction {
     public void clickNavbarBtn (String navbar) {
         WebElement navBtn = driver.findElement(navbarLoc(navbar));
         navBtn.click();
+    }
+
+    public boolean verifySignup () {
+        return signupTitle.isDisplayed();
+    }
+
+    public void setSignupUser (String username) {
+        signupUser.sendKeys(username);
+    }
+
+    public void setSignupPass (String pass) {
+        signupPass.sendKeys(pass);
+    }
+
+    public void clickSignupBtn () {
+        signupBtn.click();
+    }
+
+    public boolean verifyLogin () {
+        return loginTitle.isDisplayed();
+    }
+
+    public void setLoginUser (String username) {
+        loginUser.sendKeys(username);
+    }
+
+    public void setLoginPass (String pass) {
+        loginPass.sendKeys(pass);
+    }
+
+    public void clickLoginBtn () {
+        loginBtn.click();
+    }
+
+    public void clickCategories (String categories) {
+        WebElement catBtn = driver.findElement(catFilter(categories));
+        catBtn.click();
+    }
+
+    public void clickProducts (String product) {
+        WebElement prodBtn = driver.findElement(productLoc(product));
+        prodBtn.click();
+    }
+
+    public boolean verifyProductPage () {
+        return productTitle.isDisplayed();
+    }
+
+    public String verifyProductTitle () {
+        return productTitle.getText();
+    }
+
+    public void clickCartBtn () {
+        cartBtn.click();
+    }
+
+    public boolean verifyCartPage () {
+        return cartTitle.isDisplayed();
+    }
+
+    public void clickOrderBtn () {
+        orderBtn.click();
+    }
+
+    public boolean verifyOrderPage () {
+        return orderTitle.isDisplayed();
+    }
+
+    public void setOrderName (String name) {
+        orderName.sendKeys(name);
+    }
+
+    public void setOrderCountry (String country) {
+        orderCountry.sendKeys(country);
+    }
+
+    public void setOrderCity (String city) {
+        orderCity.sendKeys(city);
+    }
+
+    public void setOrderCard (String card) {
+        orderCard.sendKeys(card);
+    }
+
+    public void setOrderMonth (String month) {
+        orderMonth.sendKeys(month);
+    }
+
+    public void setOrderYear (String year) {
+        orderYear.sendKeys(year);
+    }
+
+    public void clickPurchaseBtn () {
+        purchaseBtn.click();
+    }
+
+    public boolean verifyPurchase () {
+        return purchaseConfirm.isDisplayed();
+    }
+
+    public void clickOKBtn () {
+        OKBtn.click();
+    }
+
+    public boolean verifyItemTable () {
+        return itemTable.isDisplayed();
     }
 }
--- src/test/resources/features/Buying.feature
+++ src/test/resources/features/Buying.feature
@@ -4,46 +4,45 @@ Feature: As a User I want to buy something in website DemoBlaze
   @Test @PositiveCase
   Scenario: New User buying products
     Given User already on DemoBlaze home page
+    And click "Sign up" on navigation bar
+    And sign up pop up will appear
+    Then input "asg002" as username and "test123" as password in sign up
+    And click sign up button
+    Then alert message will appear "Sign up successful"
+    And click "Log in" on navigation bar
+    And log in pop up will appear
+    Then input "asg002" as username and "test123" as password in log in
+    And click log in button
+    Then User will redirect to home page
+    And click "Phones" filter
+    Then click on "Sony xperia z5"
+    And User will redirect to "Sony xperia z5" product page
+    And click add to cart button
+    Then alert message will appear "Product added"
     And click "Home" on navigation bar
-#    And sign up pop up will appear
-#    Then input "asg001" as username and "test123" as password
-#    And click sign up button
-#    Then alert message will appear "Sign up successful"
-#    And click "Log in" on navigation bar
-#    And log in pop up will appear
-#    Then input "asg001" as username and "test123" as password
-#    And click log in button
-#    Then User will redirect to home page
-#    And click "Phones" filter
-#    Then click on "Sony xperia z5"
-#    And User will redirect to "Sony xperia z5" product page
-#    And click add to cart button
-#    Then alert message will appear "Product added"
-#    And click "Home" on navigation bar
-#    Then User will redirect to home page
-#    And click "Laptops" filter
-#    Then click on "Sony vaio i7"
-#    And User will redirect to "Sony vaio i7" product page
-#    And click add to cart button
-#    Then alert message will appear "Product added"
-#    And click "Home" on navigation bar
-#    Then User will redirect to home page
-#    And click "Monitors" filter
-#    Then click on "ASUS Full HD"
-#    And User will redirect to "ASUS Full HD" product page
-#    And click add to cart button
-#    Then alert message will appear "Product added"
-#    And click "Cart" on navigation bar
-#    Then User will redirect to cart page
-#    And delete an item from cart
-#    Then click place order
-#    And place order pop up will appear
-#    Then input "AndersenASG" as name, "Indonesia" as country, "Semarang" as city, "8012380" as credit card, "August" as month, and "2023" as year
-#    When click purchase
-#    Then purchase confirmation message will appear
-#    Then User will redirect to home page
-#    And click "Cart" on navigation bar
-#    Then User will redirect to cart page
-#    And cart should be empty
-#    And click "Log out" on navigation bar
-#    Then User will redirect to home page
+    Then User will redirect to home page
+    And click "Laptops" filter
+    Then click on "Sony vaio i7"
+    And User will redirect to "Sony vaio i7" product page
+    And click add to cart button
+    Then alert message will appear "Product added"
+    And click "Home" on navigation bar
+    Then User will redirect to home page
+    And click "Monitors" filter
+    Then click on "ASUS Full HD"
+    And User will redirect to "ASUS Full HD" product page
+    And click add to cart button
+    Then alert message will appear "Product added"
+    And click "Cart" on navigation bar
+    Then User will redirect to cart page
+    And click place order
+    And place order pop up will appear
+    Then input "AndersenASG" as name, "Indonesia" as country, "Semarang" as city, "8012380" as credit card, "August" as month, and "2023" as year
+    When click purchase
+    Then purchase confirmation message will appear
+    Then User will redirect to home page
+    And click "Cart" on navigation bar
+    Then User will redirect to cart page
+    And cart should be empty
+    And click "Log out" on navigation bar
+    Then User will redirect to home page
